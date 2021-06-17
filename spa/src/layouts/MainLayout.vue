e5e8ce4dd0f64441cecb95e2dd2f784cdd9a6d73
Mirla Vieira <mirla.v.oliveira@gmail.com>
2021-06-17 20:56:56 -0300

    commit inicial

---
+++ spa/src/layouts/MainLayout.vue
@@ -0,0 +1,97 @@
+<template>
+
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title class="text-h4">
+          <q-avatar>
+            <img src="../assets/svg/study.svg"/>
+          </q-avatar>
+          Cursos
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+       <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="text-white bg-footer">
+      <q-toolbar>
+         <p>
+           Cursos - todos os direitos reservados copyright; 2021
+          </p>
+      </q-toolbar>
+    </q-footer>
+    </q-layout>
+
+
+</template>
+
+<style scoped>
+  .bg-footer{
+    background-color: #0d3b69;
+  }
+</style>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'FICR',
+    caption: 'Faculdade Católica Imaculada Sra Conceição',
+    icon: 'school',
+    link: 'https://ficr.catolica.edu.br'
+  },
+  {
+    title: 'Formações',
+    caption: 'Lista dos Cursos',
+    icon: 'list',
+    link: '/formacoes'
+  },
+   {
+    title: 'Atividades',
+    caption: 'Lista de Atividades',
+    icon: 'list',
+    link: '/atividade'
+  },
+  {
+    title: 'Usuários',
+    caption: 'Lista de Usuários',
+    icon: 'account_circle',
+    link: 'https://chat.quasar.dev'
+  },
+  
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      left: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ spa/src/pages/Index.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-page class="container">
+    <div id="info">
+      <div class="row justify-center q-pa-lg">
+      <div class="col-xs-12 col-sm-12 col-md-7 col-lg-3">
+          <header><h2>Cursos de Tecnologia e Negócios Digitais</h2></header>
+          <p>Você vai estudar e aprender em uma comunidade de inquietos e inovadores.</p>
+        </div>
+        <div class="col col-xs-12 col-sm-12 col-md-5 col-lg-3">
+          <img src="../assets/estudante.svg" alt="estudante com seus livros">
+        </div>
+      </div>
+    </div>
+    <section id="descCursos" class="q-pb-xl">
+      <div class="row justify-center q-pa-lg items-start q-gutter-md">
+        <header><h3 class="text-cyan-10">FORMAÇÕES</h3></header>
+      </div>
+       
+      <div class="q-pa-md row justify-start wrap q-gutter-md">
+        <q-card class="my-card" v-for="curso in cursos" :key="curso.id">
+          <q-card-section>
+               <q-img class="curso-img-svg" :src="curso.imagem" spinner-color="white" />
+            <div class="text-h5 text-indigo-9">{{curso.titulo}}</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+           <p> {{ curso.descricao }}</p>
+           <q-btn color="primary" label="Começar"/>
+          </q-card-section>
+        </q-card>
+      </div>
+    </section> <!--#descursos-->
+      <section id="depoimentos" class="bg-indigo-1 q-pb-xl">
+   <div class="row q-pa-md q-gutter-md">
+    <div class="col col-lg-4 offset-lg-2">
+      <header><h4 class="text-blue-9 q-mt-xs">A maior plataforma brasileira de cursos de tecnologia.</h4>
+      </header>
+      <p class="text bady1">São 1272 cursos, 100% em português. E novos lançamentos todas as semanas.</p>
+  </div> <!-- /.col -->
+   <div class="col col-lg-2">
+  <q-card class="p-pa-md">
+     <q-card-section>
+       <div class="text-h5 text-indigo-9">José Pedro</div>
+      </q-card-section>
+     
+     <q-card-section class="q-pt-nome">
+       <p>Se você não sabe a ordem do que deve aprender e praticar para dominar uma tecnologia, a forma como os cursos estão organizados e estruyurados nos ajuda muito.</p>
+     </q-card-section>
+    </q-card>
+   </div> <!-- /.col -->
+   <div class="col col-lg-2">
+  <q-card class="p-pa-md">
+     <q-card-section>
+       <div class="text-h5 text-indigo-9">Mariana Souza</div>
+      </q-card-section>
+     
+     <q-card-section class="q-pt-nome">
+       <p>Consegui fazer uma mundça de carreira em uma área muito disputada e estou muito feliz:</p>
+     </q-card-section>
+    </q-card>
+   </div> <!-- /.col -->
+  </div> <!-- /.row -->
+</section> <!-- /#depoimentos -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data (){
+   return {
+  		cursos:[{
+  			id:1,
+  			titulo: "Curso HTML5 e CSS3",
+  			descricao: "O Curso de HTML5 e CSS3 vai ensinar a criar sites usando a linguagem de marcação hipertexto (HTML) e folhas de estilo em cascata (CSS).",
+        imagem:"../../svg/css-file.svg"
+  		},
+      {
+        id:2,
+        titulo: "GITHUB",
+        descricao: "Curso de Git e Github grátis prático e sem usar comando no terminal.",
+        imagem:"../../svg/git.svg"
+      },
+      {
+        id:3,
+        titulo: "Javascript",
+        descricao: "O curso de html5 e css3 vai ensinar a criar site usando a linguagem de marcação hipertexto (html) e folhas de estilo em cascata.",
+        imagem:"../../svg/coding.svg"
+      },
+      {
+        id:4,
+        titulo: "Vue.js",
+        descricao: "Construa do zero uma SPA e crie seus próprios componentes.",
+        imagem:"../../svg/device.svg"
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+div#info{
+  background-color: #2a7ae4;
+  color: #fff;
+}
+
+div#info h2{
+  font-weight: 800;
+}
+
+section#descCursos h3 {
+	margin-bottom: 0.2em;
+   font-weight: 300;
+   }
+
+div#info p {
+  font-size: 23px;
+}
+
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+
+.curso-img-svg {
+  width: 80px;
+}
+</style>
